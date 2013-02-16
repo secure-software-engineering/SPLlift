a1ca9d8eaadd82caf4513d7154841713416863e9
Eric Bodden <eric.bodden@ec-spride.de>
2013-02-16 09:31:50 -0800

    Create gh-pages branch via GitHub

--- cide-spl-ui/src/br/ufpe/cin/dfa4spl/plverifier/alloy/Constants.java
+++
@@ -1,9 +0,0 @@
-package br.ufpe.cin.dfa4spl.plverifier.alloy;
-
-public class Constants {
-
-	public static String RUN_COMMAND = "<b>run</b>";
-
-	public static String CHECK_COMMAND = "<b>check</b>";
-
-}
